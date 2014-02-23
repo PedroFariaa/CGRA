4dc71322d64bb8db8a0f437e1ee9ba5fd93bde76
PedroFariaa <pedrommlcfaria@gmail.com>
2014-02-23 23:28:01 +0000

    1st commit
    Eliminados todos os ficheiros anteriores

---
+++ Table.cpp
@@ -0,0 +1,9 @@
+#include "Table.h"
+
+
+void Table::draw() 
+{
+	this->draw();
+	glTranslated(-2.0, -2.0, 0.0);
+	this->draw();
+}
---
+++ Table.h
@@ -0,0 +1,12 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "CGFobject.h"
+
+class Table: public CGFobject {
+	public:
+		ExampleObject myUnitCube;
+		void draw();
+};
+
+#endif
